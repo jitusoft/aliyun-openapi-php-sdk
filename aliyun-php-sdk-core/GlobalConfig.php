e23d3d29b8d4b3c8c7e676fb1c0d3b60b6910ac6
szualang <liangalang@qq.com>
2018-04-15 15:44:19 +0800

    修复阿里云默认加载congfig.php配置文件导致composer出现异常。

---
+++ aliyun-php-sdk-core/GlobalConfig.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 阿里云配置文件
+ * Created by PhpStorm.
+ * User: lianglang
+ * Date: 2018-4-15
+ * Time: 15:41
+ */
+
+//config http proxy
+define('ENABLE_HTTP_PROXY', false);
+define('HTTP_PROXY_IP', '127.0.0.1');
+define('HTTP_PROXY_PORT', '8888');
